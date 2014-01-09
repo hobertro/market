55c6e245ad3e6e7b50ff492faf50d9c9125767fb
djShrek <boho527@gmail.com>
2014-01-09 00:06:20 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome To The Dota Market! :D
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DotaMarket::Application.config.secret_token = '2d8047ad1e62bf467d9d62636aa5b09cfe2d0cf3d4c36f09ba2942bb6b703aa4e171eb6b36bedae519065132363ff38702d8cd22fa077b6a2376684d5003e741'
