15f7efa8f1e637e8cef5c26d44f20f7cc60456c0
djShrek <boho527@gmail.com>
2014-04-19 11:11:05 -0700

    added .to_i to item[defindex] in creater_player_items method

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ActiveRecord::Base
     # get player items
     player_item_hash = User.get_user_items(steam_id)["result"]["items"]
     # create an array of the items based on defindex numbers
-    defindex_ids = player_item_hash.map { |item| item["defindex"]  }
+    defindex_ids = player_item_hash.map { |item| item["defindex"].to_i  }
     # find and create an array based on the defindexs in the Items table defindex 
     items = Item.where(:defindex => defindex_ids).to_a
     #items_dict = {}
