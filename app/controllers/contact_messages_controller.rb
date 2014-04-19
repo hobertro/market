ec8445ab7e6925d691d8943ea6d90378220f85e1
djShrek <boho527@gmail.com>
2014-04-18 17:10:06 -0700

    edited the contact form

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -6,14 +6,9 @@ class ContactMessagesController < ApplicationController
     @subject = params[:subject]
     @content = params[:content]
     ContactMailer.welcome_email(@name, @email, @subject, @content).deliver
+    redirect_to new_contact_message_path
   end
 
   def new
-    puts "hello"
-    @name = params[:name]
-    @email = params[:email]
-    @subject = params[:subject]
-    @content = params[:content]
-    ContactMailer.welcome_email(@name, @email, @subject, @content).deliver
   end
 end
