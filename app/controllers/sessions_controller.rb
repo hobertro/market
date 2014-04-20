13cfa6c8222ebc21f2decd3475cbc62238d5b63d
djShrek <boho527@gmail.com>
2014-04-20 16:14:03 -0700

    added error and exception handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,11 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     @user = User.from_omniauth(auth)
     if @user.user_items.empty?
-      @items = @user.create_player_items(@user.steam_id)
+      begin
+        @items = @user.create_player_items(@user.steam_id)
+      rescue
+        puts "hihi"
+      end
       # @items = @user.create_player_items
       # create item_id_if_not_exist, if exists, then return, make it indempotent
       # put flag on db on the user, put false by default, everytime you scan user items, put to true
