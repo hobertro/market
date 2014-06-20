2cbc0ab334e96717cc07b99b09b6cd464ff03973
djShrek <boho527@gmail.com>
2014-06-19 19:17:27 -0700

    took out misc. comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,19 +13,6 @@ class SearchController < ApplicationController
     puts search_params
     if search_params
        @listings = UserListing.joins(:items).where("lower(items.name) LIKE ? ", "%#{search_params.downcase}%").uniq.paginate(:page => params[:page]).per_page(10)
-       puts @listings
-       puts "#{@listings}"
-       puts @listings
-       puts @listings
-       puts @listings
-       puts "Hihi!"
-       puts @listings
-       puts "Hihi!"
-       puts "HihiHihiHihiHihi!"
-       puts "hello!"
-       puts @listings
-       puts "Hihi!"
-       @listings
     else
         render :root_path
     end
