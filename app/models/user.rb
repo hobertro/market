ac040a3b126ba41de3012f3cacf401fa1ec398e7
djShrek <boho527@gmail.com>
2014-08-27 22:38:56 -0700

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,12 +50,6 @@ class User < ActiveRecord::Base
   def self.is_relationship_blocked?(user, other_user)
     Relationship.is_blocked_relationship?(user, other_user)
   end
-                          
-
-  def reload_player_items
-    self.user_items.delete_all
-    create_player_items(self.steam_id)
-  end
 
   def have_items?
     create_player_items(steam_id) if self.user_items.empty?
@@ -68,43 +62,48 @@ class User < ActiveRecord::Base
     begin
       parsed_data(url)  # reading HTTP request using open-uri
     rescue
-      return "hihi"
+      puts "in rescue clause"
+      return 
     end
+  end
+
+  def reload_player_items
+    self.user_items.delete_all
+    create_player_items(steam_id)
   end
 
   def create_player_items(steam_id)
     # get player items
     begin
-        self.user_items.delete_all
-        player_item_hash = User.get_user_items(steam_id)["result"]["items"]
+      player_item_hash = User.get_user_items(steam_id)["result"]["items"]
     rescue
-      return "hihi"
+      puts "in rescue"
+      return
     end
-        # create an array of the items based on defindex numbers
-        defindex_ids = player_item_hash.map { |item| item["defindex"].to_s }
-        # find and create an array based on the defindexs in the Items table defindex 
-        items = Item.where(:defindex => defindex_ids).to_a
-        items.each do |item|
-          player_item_hash.each do |hash_item|
-            if hash_item["defindex"].to_s == item.defindex
-              #self.user_items.create(item_id: item.id, equipped: hash_item["equipped"],
-              #quality: hash_item["quality"], rarity: item.rarity)
-              self.user_items.create! do |ui| #ui = user item
-                ui.item_id          = item.id
-                ui.equipped         = hash_item["equipped"]
-                ui.quality          = hash_item["quality"]
-                ui.rarity           = item.rarity
-                ui.name             = item.name
-                ui.defindex         = item.defindex
-                ui.image_url        = item.image_url
-                ui.item_description = item.item_description
-                ui.item_set         = item.item_set
-              end
-            end
+    puts player_item_hash
+    # create an array of the items based on defindex numbers
+    defindex_ids = player_item_hash.map { |item| item["defindex"].to_s }
+    # find and create an array based on the defindexs in the Items table defindex 
+    items = Item.where(:defindex => defindex_ids).to_a
+    items.each do |item|
+      player_item_hash.each do |hash_item|
+        if hash_item["defindex"].to_s == item.defindex
+          self.user_items.create! do |ui| #ui = user item
+            ui.item_id          = item.id
+            ui.equipped         = hash_item["equipped"]
+            ui.quality          = hash_item["quality"]
+            ui.rarity           = item.rarity
+            ui.name             = item.name
+            ui.defindex         = item.defindex
+            ui.image_url        = item.image_url
+            ui.item_description = item.item_description
+            ui.item_set         = item.item_set
           end
         end
-        #add_attr_to_items
-        #self.user_items
+      end
+    end
+    #add_attr_to_items
+    #self.user_items
   end
 
   def self.from_omniauth(auth)
